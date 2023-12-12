397cf1996c2165dd957412d11221e0a08de0a7c6
sol-web <sesi3home@gmail.com>
2023-12-12 22:28:19 +0900

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React from 'react'
 
-import Box from "@mui/material/Box";
+import { AppBar, Container, Toolbar, Typography } from '@mui/material'
+import { Link } from 'react-router-dom'
 
 function Footer() {
-	return (
-		<footer>
-			<Box sx={{ height: "5vh" }}></Box>
-		</footer>
-	);
+  return (
+    <footer>
+      <AppBar
+        position="static"
+        style={{
+          background: '#2E3B55',
+        }}
+      >
+        <Container>
+          <Toolbar>
+            <Typography
+              variant="h6"
+              noWrap
+              component={Link}
+              style={{
+                color: 'inherit',
+                textDecoration: 'inherit',
+              }}
+              sx={{ flexGrow: 1 }}
+            >
+              중부대학교{' '}
+            </Typography>
+            <a href="https://www.joongbu.ac.kr/is/" style={{ color: 'white' }}>
+              정보보호학과
+            </a>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </footer>
+  )
 }
 
-export default Footer;
+export default Footer
